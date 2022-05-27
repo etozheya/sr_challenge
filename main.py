0f5c08c5f63731621a2f11376f9f491b329dd0a7
etozheya <pishov.nahiy@yandex.ru>
2022-05-27 08:09:08 +0200

    venv & tests init

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ from get_games import get_last_games_for_all_tournaments
 
 def main(tournaments_input, number_of_games):
     available_tournaments = get_tournaments.get()
-    print('Here is a list of available tournaments:')
-    print([t['name'] for t in available_tournaments])
+    # print('Here is a list of available tournaments:')
+    # print([t['name'] for t in available_tournaments])
     tournaments = available_tournaments
     if tournaments_input:
         tournaments = []
         for ti in tournaments_input:
             found = [at for at in available_tournaments if at['name'] == ti]
             if not found:
-                print(f'You entered invalid tournament name: {ti}.')
+                # print(f'You entered invalid tournament name: {ti}.')
                 return
     games = get_last_games_for_all_tournaments(tournaments, number_of_games)
-    for g in games.values():
-        print(g)
+    # for g in games.values():
+    #     print(g)
+    return games
 
 
 if __name__ == '__main__':
---
+++ tests/main/test_main.py
@@ -0,0 +1,28 @@
+import main
+
+
+def test_not_custom(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(main.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        main, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    games = main.main(None, 5)
+    assert games == 'games'
+
+
+def test_custom(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(main.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        main, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    games = main.main(['bl'], 5)
+    assert games == 'games'
+
+
+def test_invalid_tournament_name(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(main.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        main, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    games = main.main(['qwer'], 5)
+    assert not games
