c5e1871584e4f0311cba242fa9d1267701506b57
etozheya <pishov.nahiy@yandex.ru>
2022-05-28 19:55:50 +0200

    fix outdated tests

--- data.py
+++ data.py
@@ -10,16 +10,14 @@ def main(tournaments_input, number_of_games):
     if number_of_games < 1:
         raise UserInputError('Non-positive number was entered')
     available_tournaments = get_tournaments.get()
-    # print('Here is a list of available tournaments:')
-    # print([t['name'] for t in available_tournaments])
     tournaments = available_tournaments
     if tournaments_input:
         tournaments = []
         for ti in tournaments_input:
             found = [at for at in available_tournaments if at['name'] == ti]
-            tournaments.append(found[0])
             if not found:
                 raise UserInputError(
                     f'You entered invalid tournament name: {ti}')
+            tournaments.append(found[0])
     games = get_last_games_for_all_tournaments(tournaments, number_of_games)
     return games
---
+++ tests/data/test_data.py
@@ -0,0 +1,38 @@
+import data
+import pytest
+
+
+def test_not_custom(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(data.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        data, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    games = data.main(None, 5)
+    assert games == 'games'
+
+
+def test_custom(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(data.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        data, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    games = data.main(['bl'], 5)
+    assert games == 'games'
+
+
+def test_invalid_tournament_name(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(data.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        data, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    with pytest.raises(data.UserInputError):
+        _ = data.main(['qwer'], 5)
+
+
+def test_invalid_number_of_games(monkeypatch):
+    tournaments = [{'name': 'bl'}, {'name': 'blc'}]
+    monkeypatch.setattr(data.get_tournaments, 'get', lambda *_: tournaments)
+    monkeypatch.setattr(
+        data, 'get_last_games_for_all_tournaments', lambda *_: 'games')
+    with pytest.raises(data.UserInputError):
+        _ = data.main(['qwer'], 0)
