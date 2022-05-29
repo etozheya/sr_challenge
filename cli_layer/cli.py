17ba909ffa61808c1dec8dd3b57d65b900d80cc3
etozheya <pishov.nahiy@yandex.ru>
2022-05-29 13:28:43 +0200

    trying to fix moduleimporterror

--- cli_layer/cli.py
+++ cli_layer/cli.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 import sys
+import os
+myDir = os.getcwd()
+sys.path.append(myDir)
+
+from pathlib import Path
+path = Path(myDir)
+a=str(path.parent.absolute())
+
+sys.path.append(a)
 from datetime import datetime
 
 import click
